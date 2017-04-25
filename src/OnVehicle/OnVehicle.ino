7c21472264975e0ad0142b55ce06686cf80014b4
Tom Zhang <tomzhang1995@gmail.com>
2017-04-25 14:52:42 -0400

    Test Code of the transimiter and receiver...

--- src/OnVehicle/OnVehicle.ino
+++ src/OnVehicle/OnVehicle.ino
@@ -5,11 +5,13 @@ void setup() {
   // put your setup code here, to run once:
   SetupTransmitter(8);
   pinMode(7,INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   bool isOpen = digitalRead(7);
+  Serial.println(isOpen);
   if (isOpen)
   {
     SendMsg(101);
